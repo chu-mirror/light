aa0ee5b6772fe27196953cb5dd0e61aa63e9aef2
chu <maedaqu@gmail.com>
2025-05-10 16:37:26 +0800

    Add typeof

--- include/memgr.h
+++ include/memgr.h
@@ -7,19 +7,22 @@
 
 extern size_t alloc_count;
 
-#define NEW(p) (++alloc_count, (p) = malloc(sizeof *(p)))
+#define NEW(p) (++alloc_count, (p) = (typeof(p))malloc(sizeof *(p)))
 #define _NEW(p) ((p) = malloc(sizeof *(p)))
 
-#define CALLOC(p, len) (++alloc_count, (p) = calloc((len), sizeof *(p)))
-#define _CALLOC(p, len) ((p) = calloc((len), sizeof *(p)))
+#define CALLOC(p, len)                                           \
+    (++alloc_count, (p) = (typeof(p))calloc((len), sizeof *(p)))
+#define _CALLOC(p, len) ((p) = (typeof(p) calloc((len), sizeof *(p)))
 
-#define NEW0(p) CALLOC(p, 1)
-#define _NEW0(p) _CALLOC(p, 1)
+#define NEW0(p) CALLOC((p), 1)
+#define _NEW0(p) _CALLOC((p), 1)
 
-#define FREE(p) ((void)(assert(p != NULL), --alloc_count, free(p), (p) = NULL))
-#define _FREE(p) ((void)(assert(p != NULL), free(p), (p) = NULL))
+#define FREE(p)                                                       \
+    ((void)(assert((p) != NULL), --alloc_count, free(p), (p) = NULL))
+#define _FREE(p) ((void)(assert((p) != NULL), free(p), (p) = NULL))
 
-#define REALLOC_ARRAY(p, len) (p = reallocarray(p, sizeof *(p), len))
+#define REALLOC_ARRAY(p, len)                              \
+    ((p) = (typeof(p))reallocarray((p), sizeof *(p), len))
 
 #define MOVE(src, dest) (NEW(dest), memcpy((dest), (src), sizeof *(dest)))
 #define MOVE_ARRAY(src, dest, n)                                   \
